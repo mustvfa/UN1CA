5880fd68fb2196cd4e372eac0ff800c05af2e58c
ExtremeXT <extremext@lineageos.org>
2026-09-25 19:29:04 +0300

    a21s: misc: init

---
+++ target/a21s/patches/misc/customize.sh
@@ -0,0 +1,11 @@
+LOG "-Disabling encryption"
+# Encryption
+LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos850")
+sed -i "${LINE}s/,fileencryption=ice//g" "$WORK_DIR/vendor/etc/fstab.exynos850"
+
+# ODE
+sed -i -e "/ODE/d" -e "/keydata/d" -e "/keyrefuge/d" "$WORK_DIR/vendor/etc/fstab.exynos850"
+
+LOG_STEP_IN "- Disabling A2DP Offload"
+SET_PROP "system" persist.bluetooth.a2dp_offload.disabled "true"
+LOG_STEP_OUT

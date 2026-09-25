088fa11846c46c78fa0cd350cbb8aa644a0dd5c3
mustvfa <mustvfa@proton.me>
2026-09-25 20:12:17 +0300

    [TEMP] nuke fmradio for now

--- unica/mods/tweaks/customize.sh
+++ unica/mods/tweaks/customize.sh
@@ -2,14 +2,14 @@
 # Guard the patch as the source firmware might have this already disabled
 LOG "- Applying \"Disable app compaction\" to /system/system/framework/services.jar"
 APPLY_PATCH "system" "system/framework/services.jar" \
-    "$MODPATH/appcompactor/services.jar/0001-Disable-app-compaction.patch" &> /dev/null || true
+  "$MODPATH/appcompactor/services.jar/0001-Disable-app-compaction.patch" &>/dev/null || true
 
 # Disable FM Radio country restrictions
-if [[ "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_CHIP_VENDOR")" != "0" ]]; then
-    SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION" --delete
-
-    SMALI_PATCH "system" "system/framework/samsungfmradiolib.jar" \
-        "smali/com/samsung/frameworks/fmradio/FMRadioImpl.smali" "return" \
-        'isRadioNotSupportedInRegion()Z' \
-        'false'
-fi
+# if [[ "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_CHIP_VENDOR")" != "0" ]]; then
+#     SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION" --delete
+#
+#     SMALI_PATCH "system" "system/framework/samsungfmradiolib.jar" \
+#         "smali/com/samsung/frameworks/fmradio/FMRadioImpl.smali" "return" \
+#         'isRadioNotSupportedInRegion()Z' \
+#         'false'
+# fi
